b26df487ec71b2aace2181b675b13d8f0cfd1989
EllaLee233 <lmlll2293068536@gmail.com>
2024-10-07 14:28:53 -0400

    Initial Commit

---
+++ Monty/MontyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MontyApp.swift
+//  Monty
+//
+//  Created by apple on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MontyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
